eba72ab218ca7a8cd197ea0f328f27404aafcf1c
adlerre <rene.adler@uni-jena.de>
2016-11-21 09:03:10 +0100

    add missing semicolon

--- src/main/resources/META-INF/resources/assets/js/ie10-viewport-bug-workaround.js
+++ src/main/resources/META-INF/resources/assets/js/ie10-viewport-bug-workaround.js
@@ -6,18 +6,13 @@
 
 // See the Getting Started docs for more information:
 // http://getbootstrap.com/getting-started/#support-ie10-width
+(function() {
+	'use strict';
 
-(function () {
-  'use strict';
-
-  if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
-    var msViewportStyle = document.createElement('style')
-    msViewportStyle.appendChild(
-      document.createTextNode(
-        '@-ms-viewport{width:auto!important}'
-      )
-    )
-    document.querySelector('head').appendChild(msViewportStyle)
-  }
+	if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
+		var msViewportStyle = document.createElement('style')
+		msViewportStyle.appendChild(document.createTextNode('@-ms-viewport{width:auto!important}'));
+		document.querySelector('head').appendChild(msViewportStyle)
+	}
 
 })();
