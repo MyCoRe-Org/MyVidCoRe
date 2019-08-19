b3fae668cefba4ca98e60028dc6d9931083e31d1
René Adler <rene.adler@uni-jena.de>
2019-08-19 10:00:32 +0200

    reuse import

--- src/main/java/org/mycore/vidconv/frontend/annotation/CacheControl.java
+++ src/main/java/org/mycore/vidconv/frontend/annotation/CacheControl.java
@@ -22,7 +22,6 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import java.util.concurrent.TimeUnit;
 
@@ -42,129 +41,129 @@ import java.util.concurrent.TimeUnit;
  */
 public @interface CacheControl {
 
-	/**
-	 * sets {@code private} directive
-	 * 
-	 * @see <a href=
-	 *      "https://tools.ietf.org/html/rfc7234#section-5.2.2.6">private
-	 *      definition</a>
-	 */
-	FieldArgument private_() default @FieldArgument();
+    /**
+     * sets {@code private} directive
+     * 
+     * @see <a href=
+     *      "https://tools.ietf.org/html/rfc7234#section-5.2.2.6">private
+     *      definition</a>
+     */
+    FieldArgument private_() default @FieldArgument();
 
-	/**
-	 * sets {@code no-cache} directive
-	 * 
-	 * @see <a href=
-	 *      "https://tools.ietf.org/html/rfc7234#section-5.2.2.2">no-cache
-	 *      definition</a>
-	 */
-	FieldArgument noCache() default @FieldArgument;
+    /**
+     * sets {@code no-cache} directive
+     * 
+     * @see <a href=
+     *      "https://tools.ietf.org/html/rfc7234#section-5.2.2.2">no-cache
+     *      definition</a>
+     */
+    FieldArgument noCache() default @FieldArgument;
 
-	/**
-	 * if {@link #noCache()}, sets {@code noCache} directive argument to these
-	 * values
-	 * 
-	 * @see <a href=
-	 *      "https://tools.ietf.org/html/rfc7234#section-5.2.2.2">no-cache
-	 *      definition</a>
-	 */
-	String[] noCacheFields() default {};
+    /**
+     * if {@link #noCache()}, sets {@code noCache} directive argument to these
+     * values
+     * 
+     * @see <a href=
+     *      "https://tools.ietf.org/html/rfc7234#section-5.2.2.2">no-cache
+     *      definition</a>
+     */
+    String[] noCacheFields() default {};
 
-	/**
-	 * if true, sets {@code no-store} directive
-	 * 
-	 * @see <a href=
-	 *      "https://tools.ietf.org/html/rfc7234#section-5.2.2.3">no-store
-	 *      definition</a>
-	 */
-	boolean noStore() default false;
+    /**
+     * if true, sets {@code no-store} directive
+     * 
+     * @see <a href=
+     *      "https://tools.ietf.org/html/rfc7234#section-5.2.2.3">no-store
+     *      definition</a>
+     */
+    boolean noStore() default false;
 
-	/**
-	 * if true, sets {@code no-transform} directive
-	 * 
-	 * @see <a href=
-	 *      "https://tools.ietf.org/html/rfc7234#section-5.2.2.4">no-transform
-	 *      definition</a>
-	 */
-	boolean noTransform() default false;
+    /**
+     * if true, sets {@code no-transform} directive
+     * 
+     * @see <a href=
+     *      "https://tools.ietf.org/html/rfc7234#section-5.2.2.4">no-transform
+     *      definition</a>
+     */
+    boolean noTransform() default false;
 
-	/**
-	 * if true, sets {@code must-revalidate} directive
-	 * 
-	 * @see <a href=
-	 *      "https://tools.ietf.org/html/rfc7234#section-5.2.2.1">must-revalidate
-	 *      definition</a>
-	 */
-	boolean mustRevalidate() default false;
+    /**
+     * if true, sets {@code must-revalidate} directive
+     * 
+     * @see <a href=
+     *      "https://tools.ietf.org/html/rfc7234#section-5.2.2.1">must-revalidate
+     *      definition</a>
+     */
+    boolean mustRevalidate() default false;
 
-	/**
-	 * if true, sets {@code proxy-revalidate} directive
-	 * 
-	 * @see <a href=
-	 *      "https://tools.ietf.org/html/rfc7234#section-5.2.2.7">proxy-revalidate
-	 *      definition</a>
-	 */
-	boolean proxyRevalidate() default false;
+    /**
+     * if true, sets {@code proxy-revalidate} directive
+     * 
+     * @see <a href=
+     *      "https://tools.ietf.org/html/rfc7234#section-5.2.2.7">proxy-revalidate
+     *      definition</a>
+     */
+    boolean proxyRevalidate() default false;
 
-	/**
-	 * if true, sets {@code public} directive
-	 * 
-	 * @see <a href="https://tools.ietf.org/html/rfc7234#section-5.2.2.5">public
-	 *      definition</a>
-	 */
-	boolean public_() default false;
+    /**
+     * if true, sets {@code public} directive
+     * 
+     * @see <a href="https://tools.ietf.org/html/rfc7234#section-5.2.2.5">public
+     *      definition</a>
+     */
+    boolean public_() default false;
 
-	/**
-	 * Sets {@code max-age} directive
-	 * 
-	 * @see <a href=
-	 *      "https://tools.ietf.org/html/rfc7234#section-5.2.2.8">max-age
-	 *      definition</a>
-	 */
-	Age maxAge() default @Age(time = -1, unit = TimeUnit.SECONDS);
+    /**
+     * Sets {@code max-age} directive
+     * 
+     * @see <a href=
+     *      "https://tools.ietf.org/html/rfc7234#section-5.2.2.8">max-age
+     *      definition</a>
+     */
+    Age maxAge() default @Age(time = -1, unit = TimeUnit.SECONDS);
 
-	/**
-	 * Sets {@code s-maxage} directive
-	 * 
-	 * @see <a href=
-	 *      "https://tools.ietf.org/html/rfc7234#section-5.2.2.9">s-maxage
-	 *      definition</a>
-	 */
-	Age sMaxAge() default @Age(time = -1, unit = TimeUnit.SECONDS);
+    /**
+     * Sets {@code s-maxage} directive
+     * 
+     * @see <a href=
+     *      "https://tools.ietf.org/html/rfc7234#section-5.2.2.9">s-maxage
+     *      definition</a>
+     */
+    Age sMaxAge() default @Age(time = -1, unit = TimeUnit.SECONDS);
 
-	/**
-	 * Sets further Cache-Control Extensions
-	 * 
-	 * @see <a href="https://tools.ietf.org/html/rfc7234#section-5.2.3">Cache
-	 *      Control Extensions</a>
-	 */
-	Extension[] extensions() default {};
+    /**
+     * Sets further Cache-Control Extensions
+     * 
+     * @see <a href="https://tools.ietf.org/html/rfc7234#section-5.2.3">Cache
+     *      Control Extensions</a>
+     */
+    Extension[] extensions() default {};
 
-	@Retention(RetentionPolicy.RUNTIME)
-	@Target(ElementType.ANNOTATION_TYPE)
-	public static @interface Age {
-		long time();
+    @Retention(RUNTIME)
+    @Target(ElementType.ANNOTATION_TYPE)
+    public static @interface Age {
+        long time();
 
-		TimeUnit unit() default TimeUnit.MINUTES;
+        TimeUnit unit() default TimeUnit.MINUTES;
 
-	}
+    }
 
-	public static @interface FieldArgument {
-		/**
-		 * if true, this directive is present in header value
-		 */
-		boolean active() default false;
+    public static @interface FieldArgument {
+        /**
+         * if true, this directive is present in header value
+         */
+        boolean active() default false;
 
-		/**
-		 * if {@link #active()}, sets directive argument to these values
-		 */
-		String[] fields() default {};
-	}
+        /**
+         * if {@link #active()}, sets directive argument to these values
+         */
+        String[] fields() default {};
+    }
 
-	public static @interface Extension {
-		String directive();
+    public static @interface Extension {
+        String directive();
 
-		String argument() default "";
-	}
+        String argument() default "";
+    }
 
 }
