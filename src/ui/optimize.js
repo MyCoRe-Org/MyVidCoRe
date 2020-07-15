0957e857f9175804260330880decfd464306b929
René Adler <rene.adler@uni-jena.de>
2020-07-15 13:24:26 +0200

    improve image optimization

--- src/ui/optimize.js
+++ src/ui/optimize.js
@@ -1,6 +1,8 @@
 const { Console } = require("console");
+const os = require("os");
 const fs = require("fs");
 const path = require("path");
+const pLimit = require("p-limit");
 const imagemin = require("imagemin");
 const imageminMozjpeg = require("imagemin-mozjpeg");
 const imageminPngquant = require("imagemin-pngquant");
@@ -9,7 +11,14 @@ const imageminSvgo = require("imagemin-svgo");
 
 const logger = new Console(process.stdout, process.stderr);
 
+const limit = pLimit(os.cpus().length);
+
 const distDir = process.argv[2];
+
+const humanFileSize = (size) => {
+    const i = Math.floor(Math.log(size) / Math.log(1024));
+    return (size / Math.pow(1024, i)).toFixed(2) * 1 + ["B", "kB", "MB", "GB", "TB"][i];
+};
 
 const walk = (dir, done) => {
     let results = [];
@@ -56,9 +65,9 @@ walk(distDir, (err, files) => {
     }
 
     const images = files.filter((f) => /.*\.(gif|jpe?g|png|svg)$/.test(f));
-    images.forEach((f) => {
+    const promises = images.map((f) => {
         const fsize = fs.statSync(f).size;
-        imagemin([f], {
+        return limit(() => imagemin([f], {
             destination: path.resolve(f, ".."),
             plugins: [
                 imageminMozjpeg({ quality: 70, dct: "float" }),
@@ -69,8 +78,12 @@ walk(distDir, (err, files) => {
         }).then((done) => {
             if (done[0]) {
                 const afsize = fs.statSync(done[0].destinationPath).size;
-                logger.log("optimized " + done[0].destinationPath + " (" + fsize + " -> " + afsize + ").");
+                logger.log("optimized " + done[0].destinationPath + " (" + humanFileSize(fsize) + " -> " + humanFileSize(afsize) + ").");
             }
-        }).catch(logger.error);
+        }).catch(logger.error));
     });
+
+    (async () => {
+        await Promise.all(promises);
+    })();
 });
